03c2971b5036ad0c177a8ec4ddaa66b6b6cf09bc
Frknk <frknk1@protonmail.com>
2024-12-24 20:23:52 -0500

    event view details

--- src/components/EventDetails.svelte
+++ src/components/EventDetails.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import service from "../lib/service";
+    import Footer from "./Footer.svelte";
 
     let id;
     let response;
@@ -8,7 +9,6 @@
     onMount(async () => {
         id = window.location.pathname.split("/").pop();
         response = (await service.getEvent(id)).data;
-        console.log(response.name);
 
         if (!response) {
             window.location.href = "/";
@@ -18,15 +18,18 @@
 
 <section class="flex min-h-screen font-inter flex-col">
     <div class="mt-16 flex flex-col">
-        <h1 class="font-roboto-mono font-bold text-3xl mb-4">{response?.description}</h1>
-        <hr class="border-black border-t border-0 w-full mb-8">
-
+        <h1 class="font-roboto-mono font-bold text-3xl mb-2">
+            {response?.description}
+        </h1>
+        <p class="mb-4">{new Date(response?.createAt).toLocaleDateString()}</p>
+        <hr class="border-black border-t border-0 w-full mb-8" />
         <div class="flex flex-row justify-between">
             <div class="flex flex-col">
-                
                 <div class="flex flex-col mb-8">
                     <p class="mb-4 text-sm">CATEGORIA:</p>
-                    <p class="text-2xl pl-1">{response?.category.name.toUpperCase()}</p>
+                    <p class="text-2xl pl-1">
+                        {response?.category.name.toUpperCase()}
+                    </p>
                 </div>
                 <div class="flex flex-col mb-8">
                     <p class="mb-4 text-sm">EVENTO:</p>
@@ -34,15 +37,30 @@
                 </div>
                 <div class="flex flex-col mb-8">
                     <p class="mb-4 text-sm">PRECIO:</p>
-                    <button class="px-2 py-4 rounded-full bg-orange-500 border text-sm font-medium text-white">${response?.price}</button>
+                    <button
+                        class="px-2 py-4 rounded-full bg-orange-500 border text-sm font-medium text-white"
+                        >${response?.price}</button
+                    >
                 </div>
             </div>
             <div>
-                <img src={`https://picsum.photos/400/300?random={response?.eventId}`} alt="Event Img" class="w-96 h-72 object-cover">
+                <img
+                    src={`https://picsum.photos/400/300?random={response?.eventId}`}
+                    alt="Event Img"
+                    class="w-96 h-72 object-cover"
+                />
             </div>
         </div>
-        <hr class="border-black border-t border-0 w-full mb-8">
-
+        <hr class="border-black border-t border-0 w-full mb-8" />
+        <div class="flex flex-col mb-8">
+            <p class="mb-8">
+                {response?.comment}
+            </p>
+            <p>
+                {response?.comment2}
+            </p>
+        </div>
         <p>{JSON.stringify(response, null, 2)}</p>
     </div>
+    <Footer />
 </section>
